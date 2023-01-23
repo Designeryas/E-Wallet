ba2f906fc63a7380c7debfd90b30932c822a7c21
designeryas <designaryas16@gmail.com>
2023-01-23 14:53:58 +0330

    create repository

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // const height_img = $('.man-city-logo img').height();
+    // $('.world-cup-logo img').css({'height': height_img})
+    // $(window).on('resize', function(){
+    //     const height_img = $('.man-city-logo img').height();
+    //     $('.world-cup-logo img').css({'height': height_img})
+    // });
+})
+function qrcodeClicked() {
+    console.log("clicked")
+}
+function nextStep() {
+    $('#step1').addClass('d-none');
+    $('#step2').removeClass('d-none');
+}
