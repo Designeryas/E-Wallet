dde39a0163f4e79f6185fdb2db46aac0557d2b7e
designeryas <designaryas16@gmail.com>
2023-01-25 13:50:57 +0330

    complete profile page-add some changes

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,45 @@
-$(document).ready(function(){
-    // const height_img = $('.man-city-logo img').height();
-    // $('.world-cup-logo img').css({'height': height_img})
-    // $(window).on('resize', function(){
-    //     const height_img = $('.man-city-logo img').height();
-    //     $('.world-cup-logo img').css({'height': height_img})
-    // });
-})
+$(document).ready(function () {
+  // const height_img = $('.man-city-logo img').height();
+  // $('.world-cup-logo img').css({'height': height_img})
+  // $(window).on('resize', function(){
+  //     const height_img = $('.man-city-logo img').height();
+  //     $('.world-cup-logo img').css({'height': height_img})
+  // });
+  $('#send .back').click(function(e) {
+    e.preventDefault();
+    var url = $(location).attr("href");
+    var isStepTwo = $("#send_step1").has("d-none");
+    if(isStepTwo) {
+        $("#step1").removeClass("d-none");
+        $("#step2").addClass("d-none");
+    } else {
+        parts = url.split("/");
+        last_part = parts[parts.length - 1];
+        url = url.replace(last_part, "Home.html");
+        $(location).attr("href", url);
+    }
+  })
+});
 function qrcodeClicked() {
-    console.log("clicked")
+  console.log("clicked");
 }
 function nextStep() {
-    $('#step1').addClass('d-none');
-    $('#step2').removeClass('d-none');
+  $("#step1").addClass("d-none");
+  $("#step2").removeClass("d-none");
 }
 function sendNextStep() {
-    $('#send_step1').addClass('d-none');
-    $('#send_step2').removeClass('d-none');
+  $("#send_step1").addClass("d-none");
+  $("#send_step2").removeClass("d-none");
 }
+function register() {
+  console.log("new url", url);
+}
+
+$("#register_form").submit(function (e) {
+    e.preventDefault();
+  var url = $(location).attr("href");
+  parts = url.split("/");
+  last_part = parts[parts.length - 1];
+  url = url.replace(last_part, "Home.html");
+  $(location).attr("href", url);
+});
